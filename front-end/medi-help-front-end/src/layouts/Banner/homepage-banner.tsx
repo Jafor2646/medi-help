492d934e6b821e88dd795757d3005f8454eb9465
Jafor Mohammad <jaformd7492@gmail.com>
2023-09-15 03:37:27 +0600

    Resized the banner image - jafor

--- front-end/medi-help-front-end/src/layouts/Banner/homepage-banner.tsx
+++ front-end/medi-help-front-end/src/layouts/Banner/homepage-banner.tsx
@@ -2,8 +2,8 @@ import image from "./../../images/Banner-images/current-banner.jpg";
 export const ImageContainer = () => {
   return (
     <div className="container mt-1">
-      <div className="d-flex justify-content-center">
-          <img src={image} alt={"Banner"} height="100" />
+      <div className="d-flex justify-content-left">
+          <img src={image} alt={"Banner"} height="100" width={"1000"} />
       </div>
     </div>
 )}
