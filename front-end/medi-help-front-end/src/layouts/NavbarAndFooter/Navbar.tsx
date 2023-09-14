3ac92d6fcd8d30727d5a43a98f851c75a96b1b2e
Md. Shariar Islam Shuvo <shariaislamshuvo@gmail.com>
2023-09-15 01:12:52 +0600

    Nav Bar basic build -shuvo

--- front-end/medi-help-front-end/src/layouts/NavbarAndFooter/Navbar.tsx
+++ front-end/medi-help-front-end/src/layouts/NavbarAndFooter/Navbar.tsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
           </a>
         </span>
 
-        <form className="d-flex flex-fill">
+        <form className="d-flex flex-grow-1">
           <input
             className="form-control me-2"
             type="search"
@@ -25,7 +25,7 @@ export const Navbar = () => {
         </form>
 
         <button
-          className="navbar-toggler"
+          className="navbar-toggler m-1"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNavDropdown"
@@ -36,14 +36,6 @@ export const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          {/* <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                {" "}
-                Search Books
-              </a>
-            </li>
-          </ul> */}
           <ul className="navbar-nav ms-auto">
             <li className="nav-item m-1">
               <a type="button" className="btn btn-outline-dark" href="#">
