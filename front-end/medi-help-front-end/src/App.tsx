c4dac81b4c211d758cc5114bc54ed6c528a3507e
Jafor Mohammad <jaformd7492@gmail.com>
2023-09-15 03:02:18 +0600

    Added banner - Jafor

--- front-end/medi-help-front-end/src/App.tsx
+++ front-end/medi-help-front-end/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { ImageContainer } from './layouts/Banner/Ad';
 
 function App() {
   return (
-    <Navbar/>
+    <>
+    <Navbar />
+    <ImageContainer />
+    </>
   );
 }
 
