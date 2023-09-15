8872ca49c0dc12c6506f255e4ad4acea4bd7ebf8
Jafor Mohammad <jaformd7492@gmail.com>
2023-09-16 03:08:45 +0600

    Made general user profile - jafor

--- front-end/medi-help-front-end/src/App.tsx
+++ front-end/medi-help-front-end/src/App.tsx
@@ -3,12 +3,17 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import {HomePage} from "./layouts/HomePage/HomePage";
 import {Footer} from "./layouts/NavbarAndFooter/Footer";
+import { UserProfile } from './layouts/Profiles/UserProfile/UserProfile';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <HomePage/>
+      {/* <div className="col-lg">
+        <UserProfile/>
+      </div> */}
+      
       <Footer/>
     </div>
   );
--- front-end/medi-help-front-end/src/layouts/Profiles/UserProfile/UserProfile.tsx
+++ front-end/medi-help-front-end/src/layouts/Profiles/UserProfile/UserProfile.tsx
@@ -1,7 +1,31 @@
+import { ThreadViewerHomepage } from "../../HomePage/HomeHero/ThreadViewerHomepage/ThreadViewerHomepage";
+
 export const UserProfile = () => {
-    return (
-        <div>
-            Profile Design here
+  return (
+    <div>
+      <div className="card-body">
+        <div className="card shadow d-flex flex-row mb-2">
+          <img
+            className="img-fluid p-5"
+            src={require("./../../../images/DoctorOfTheDay-image/DoctorOfTheDay.jpeg")}
+            sizes="(max-width: 1080px) 100vw, 1080px"
+            width="500"
+            height="100"
+            alt="Profile Images"
+            loading="lazy"
+          />
+          <div className="card-body">
+            <div className="lc-block p-3">
+              <h5 className="m-2">Name: Shariar Islam Shuvo</h5>
+              <h5 className="m-2">UserName: Shariarislam1</h5>
+              <h5 className="m-2">email: shariarshuvo1@gmail.com</h5>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+
+      <ThreadViewerHomepage />
+    </div>
+  );
 };
+
