9d4b95bd243f005f0652d084dd02683516180ff3
Jafor Mohammad <jaformd7492@gmail.com>
2023-09-15 19:47:30 +0600

    Built Doctor Of The day of hero section - jafor

--- front-end/medi-help-front-end/src/layouts/HomePage/HomeHero/HomeHero.tsx
+++ front-end/medi-help-front-end/src/layouts/HomePage/HomeHero/HomeHero.tsx
@@ -1,16 +1,18 @@
+import { DoctorOfTheDay } from "./DoctorOfTheDay/DoctorOfTheDay"
+
 export const HomeHero = () => {
     return (
         <div>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-9 my-auto p-5">
+                    <div className="col-md-8 my-auto p-5">
                         <div>
                             Here goes Thread Container
                         </div>
                     </div>
-                    <div className="col-sm my-auto p-5">
-                        <div>
-                            Here goes Doctor Of The Day Container
+                    <div className="col-md my-auto p-5" style={{width: '300px', height:'200px'}}>
+                        <div className="row-md-5">
+                            <DoctorOfTheDay/>
                         </div>
                     </div>
                 </div>
