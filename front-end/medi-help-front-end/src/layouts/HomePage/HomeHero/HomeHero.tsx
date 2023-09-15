56fe61630fbff24b70fb6f82d92df946f2475a22
Md. Shariar Islam Shuvo <shariaislamshuvo@gmail.com>
2023-09-15 18:51:02 +0600

    Homepage Hero updated -shuvo

--- front-end/medi-help-front-end/src/layouts/HomePage/HomeHero/HomeHero.tsx
+++ front-end/medi-help-front-end/src/layouts/HomePage/HomeHero/HomeHero.tsx
@@ -3,13 +3,13 @@ export const HomeHero = () => {
         <div>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-lg-9 my-auto p-5">
-                        <div className="lc-block">
+                    <div className="col-md-9 my-auto p-5">
+                        <div>
                             Here goes Thread Container
                         </div>
                     </div>
-                    <div className="col-sm  my-auto p-5">
-                        <div className="lc-block">
+                    <div className="col-sm my-auto p-5">
+                        <div>
                             Here goes Doctor Of The Day Container
                         </div>
                     </div>
