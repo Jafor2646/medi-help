02c95293470e97b6c01f53ac13aff10081b68961
Md. Shariar Islam Shuvo <shariaislamshuvo@gmail.com>
2023-09-15 18:26:23 +0600

    Homepage Hero added

---
+++ front-end/medi-help-front-end/src/layouts/HomePage/HomeHero/HomeHero.tsx
@@ -0,0 +1,20 @@
+export const HomeHero = () => {
+    return (
+        <div>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-lg-9 my-auto p-5">
+                        <div className="lc-block">
+                            Here goes Thread Container
+                        </div>
+                    </div>
+                    <div className="col-sm  my-auto p-5">
+                        <div className="lc-block">
+                            Here goes Doctor Of The Day Container
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )}
--- front-end/medi-help-front-end/src/layouts/HomePage/HomePage.tsx
+++ front-end/medi-help-front-end/src/layouts/HomePage/HomePage.tsx
@@ -1,8 +1,11 @@
 import {HomePageBanner} from "./Banner/HomePageBanner";
+import {HomeHero} from "./HomeHero/HomeHero";
 
 export const HomePage = () => {
     return (
         <div>
             <HomePageBanner/>
+
+            <HomeHero/>
         </div>
     )}
