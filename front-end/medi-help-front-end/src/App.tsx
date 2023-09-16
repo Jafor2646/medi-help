b56f9ea94cb2a81a021e7f90de0cbf5da99a1628
Jafor Mohammad <jaformd7492@gmail.com>
2023-09-16 16:18:47 +0600

    Changes in app.tsx - jafor

--- front-end/medi-help-front-end/src/App.tsx
+++ front-end/medi-help-front-end/src/App.tsx
@@ -10,9 +10,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      {/*<UserProfile/>*/}
-      {/*<HomePage />*/}
-      <HospitalProfile/>
+      <HomePage/>
       <Footer />
     </div>
   );
