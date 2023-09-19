693cc4e920fd525dfb6c86cec4ad5479921ed949
Md. Shariar Islam Shuvo <shariaislamshuvo@gmail.com>
2023-09-19 19:36:13 +0600

    disabled security -shuvo

--- front-end/medi-help-front-end/src/App.tsx
+++ front-end/medi-help-front-end/src/App.tsx
@@ -5,6 +5,8 @@ import { HomePage } from "./layouts/HomePage/HomePage";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { UserProfile } from "./layouts/Profiles/UserProfile/UserProfile";
 import {HospitalProfile} from "./layouts/Profiles/HospitalProfile/HospitalProfile";
+import {SearchPage} from "./layouts/SearchPage/SearchPage";
+import {DoctorProfile} from "./layouts/Profiles/DoctorProfile/DoctorProfile";
 
 function App() {
   return (
