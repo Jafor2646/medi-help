a52cb5d8bc5d6bac0139c8252fe8f04a58f7c983
Md. Shariar Islam Shuvo <shariaislamshuvo@gmail.com>
2023-09-20 02:43:27 +0600

    Updated models -shuvo

--- front-end/medi-help-front-end/src/models/ThreadModel.ts
+++ front-end/medi-help-front-end/src/models/ThreadModel.ts
@@ -9,16 +9,16 @@ class ThreadModel {
     threadUpvote?: number;
     threadDownvote?: number;
 
-    constructor(thread_id: number, uploader_id: string, thread_title: string, thread_body: string, thread_date: Date, thread_view: number,thread_trend_view: number, thread_upvote: number, thread_downvote: number) {
-        this.threadId = thread_id;
-        this.uploaderId = uploader_id;
-        this.threadTitle = thread_title;
-        this.threadBody = thread_body;
-        this.threadDate = thread_date;
-        this.threadView = thread_view;
-        this.threadTrendView = thread_trend_view;
-        this.threadUpvote = thread_upvote;
-        this.threadDownvote = thread_downvote;
+    constructor(threadId: number, uploaderId: string, threadTitle: string, threadBody: string, threadDate: Date, threadView: number,threadTrendView: number, threadUpvote: number, threadDownvote: number) {
+        this.threadId = threadId;
+        this.uploaderId = uploaderId;
+        this.threadTitle = threadTitle;
+        this.threadBody = threadBody;
+        this.threadDate = threadDate;
+        this.threadView = threadView;
+        this.threadTrendView = threadTrendView;
+        this.threadUpvote = threadUpvote;
+        this.threadDownvote = threadDownvote;
     }
 }
 
---
+++ front-end/medi-help-front-end/src/models/UserModel.ts
@@ -0,0 +1,23 @@
+class UserModel {
+    userId: string;
+    userName: string;
+    email: string;
+    password: string;
+    address: string;
+    phone?: string;
+    userType?: string;
+    picture?: string;
+
+    constructor(userId: string, userName: string, email: string, password: string, address: string, phone: string, userType: string, picture: string) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+        this.phone = phone;
+        this.userType = userType;
+        this.picture = picture;
+    }
+}
+
+export default UserModel;
