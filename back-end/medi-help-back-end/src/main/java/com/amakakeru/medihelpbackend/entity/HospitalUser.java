af49121f4f487f505e32d0b341329425c144d837
Jafor Mohammad <jaformd7492@gmail.com>
2023-09-19 19:27:43 +0600

    Hospital extra info created - jafor

--- back-end/medi-help-back-end/src/main/java/com/amakakeru/medihelpbackend/entity/HospitalUser.java
+++ back-end/medi-help-back-end/src/main/java/com/amakakeru/medihelpbackend/entity/HospitalUser.java
@@ -1,2 +1,19 @@
-package com.amakakeru.medihelpbackend.entity;public class HospitalUser {
+package com.amakakeru.medihelpbackend.entity;
+
+import lombok.Data;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "hospital_extra_info")
+@Data
+public class HospitalUser {
+    @Id
+    @Column(name = "hospital_user_id")
+    private String hospital_user_id;
+
+    @Column(name = "website")
+    private String website;
+
+    @Column(name = "bio")
+    private String bio;
 }
