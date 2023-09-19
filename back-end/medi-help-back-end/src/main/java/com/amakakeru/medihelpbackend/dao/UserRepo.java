313f547fad40686868a213908820316f1082652b
Jafor Mohammad <jaformd7492@gmail.com>
2023-09-19 19:25:26 +0600

    Hospital User Extra info entity created - Jafor

--- back-end/medi-help-back-end/src/main/java/com/amakakeru/medihelpbackend/dao/UserRepo.java
+++ back-end/medi-help-back-end/src/main/java/com/amakakeru/medihelpbackend/dao/UserRepo.java
@@ -3,5 +3,5 @@ package com.amakakeru.medihelpbackend.dao;
 import com.amakakeru.medihelpbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepo extends JpaRepository<User, String> {
+public interface UserRepo extends JpaRepository<User, String> {
 }
---
+++ back-end/medi-help-back-end/src/main/java/com/amakakeru/medihelpbackend/entity/HospitalUser.java
@@ -0,0 +1,2 @@
+package com.amakakeru.medihelpbackend.entity;public class HospitalUser {
+}
