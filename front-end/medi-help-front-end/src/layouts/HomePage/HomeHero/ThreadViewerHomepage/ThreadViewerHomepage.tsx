25dbe09f3e23c49963fb79bf82dcb56b7f14a147
Md. Shariar Islam Shuvo <shariaislamshuvo@gmail.com>
2023-09-21 05:24:20 +0600

    Sort by type added-shuvo

--- front-end/medi-help-front-end/src/layouts/HomePage/HomeHero/ThreadViewerHomepage/ThreadViewerHomepage.tsx
+++ front-end/medi-help-front-end/src/layouts/HomePage/HomeHero/ThreadViewerHomepage/ThreadViewerHomepage.tsx
@@ -15,12 +15,14 @@ export const ThreadViewerHomepage = () => {
   const [ThreadsPerPage] = useState(15);
   const [totalAmountOfThreads, setTotalAmountOfThreads] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
+  const [categorySelection, setCategorySelection] = useState('Trending');
+  const [searchUrl, setSearchUrl] = useState(`&sort=threadTrendView,desc`);
 
   useEffect(() => {
     const fetchThreads = async () => {
-      const baseUrl: string = "http://localhost:8080/api/threads";
+      const baseUrl: string = "http://localhost:8080/api";
 
-      const url: string = `${baseUrl}?page=${currentPage-1}&size=${ThreadsPerPage}`;
+      const url: string = `${baseUrl}/threads?page=${currentPage-1}&size=${ThreadsPerPage}${searchUrl}`;
 
       const response = await fetch(url);
 
@@ -40,17 +42,16 @@ export const ThreadViewerHomepage = () => {
       for (const key in responseData) {
 
         let user_id: string = responseData[key].uploaderId;
-        const resp = await fetch(`http://localhost:8080/api/users/${user_id}`);
+        const resp = await fetch(`${baseUrl}/users/${user_id}`);
         const respData = await resp.json();
 
         let current_date: string = responseData[key].threadDateTxt;
-        const topicResp = await fetch(`http://localhost:8080/api/threadTopics/search/findByUploaderIdAndThreadDateTopicTxt?uploaderId=${user_id}&threadDateTopicTxt=${current_date}`);
+        const topicResp = await fetch(`${baseUrl}/threadTopics/search/findByUploaderIdAndThreadDateTopicTxt?uploaderId=${user_id}&threadDateTopicTxt=${current_date}`);
         const topicRespJson = await topicResp.json();
         const topicRespData =  topicRespJson._embedded.threadTopics;
         let topic_array: string[] = [];
         for (const topic in topicRespData){
           topic_array.push(topicRespData[topic].topicTitle);
-          console.log(topicRespData[topic].topicTitle);
         }
 
         loadedThreads.push({
@@ -73,8 +74,8 @@ export const ThreadViewerHomepage = () => {
     fetchThreads().catch((error: any) => {
       setHttpError(error.message);
     })
-    window.scrollTo(0, 0);
-  }, [currentPage]);
+    // window.scrollTo(0, 0);
+  }, [currentPage, searchUrl]);
 
   if (isLoading) {
     return (
@@ -97,22 +98,67 @@ export const ThreadViewerHomepage = () => {
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
+  const categoryField = (value: string) => {
+    if (value === 'Newest First') {
+      setCategorySelection(value);
+      setSearchUrl(`&sort=threadDate`);
+    }
+    else if (value === 'Oldest First') {
+      setCategorySelection(value);
+      setSearchUrl(`&sort=threadDate,desc`);
+    }
+    else if (value === 'Most Voted') {
+      setCategorySelection(value);
+      setSearchUrl(`&sort=threadUpvote,desc`);
+    }
+    else if (value === 'Most Viewed') {
+      setCategorySelection(value);
+      setSearchUrl(`&sort=threadView,desc`);
+    }
+    else {
+      setCategorySelection('Trending');
+      setSearchUrl(`&sort=threadTrendView,desc`);
+    }
+  }
+
   return (
       <div className={"home-thread-bg shadow"}>
         <div className="container-fluid">
           <div className="d-flex justify-content-between">
-            <div className="p-2">
-              <select
-                  className="form-select form-select-md"
-                  aria-label="Default select example"
-              >
-                <option selected>Trending</option>
-                <option value="1">Newest First</option>
-                <option value="2">Oldest First</option>
-                <option value="3">Most Voted</option>
-                <option value="3">Most Viewed</option>
-              </select>
-            </div>
+              <div className='dropdown p-2'>
+                <button className='btn bg-white dropdown-toggle' type='button'
+                        id='dropdownMenuButton1' data-bs-toggle='dropdown'
+                        aria-expanded='false'>
+                  {categorySelection}
+                </button>
+                <ul className='dropdown-menu' aria-labelledby='dropdownMenuButton1'>
+                  <li onClick={() => categoryField('Trending')}>
+                    <a className='dropdown-item' href='#'>
+                      Trending
+                    </a>
+                  </li>
+                  <li onClick={() => categoryField('Newest First')}>
+                    <a className='dropdown-item' href='#'>
+                      Newest First
+                    </a>
+                  </li>
+                  <li onClick={() => categoryField('Oldest First')}>
+                    <a className='dropdown-item' href='#'>
+                      Oldest First
+                    </a>
+                  </li>
+                  <li onClick={() => categoryField('Most Voted')}>
+                    <a className='dropdown-item' href='#'>
+                      Most Voted
+                    </a>
+                  </li>
+                  <li onClick={() => categoryField('Most Viewed')}>
+                    <a className='dropdown-item' href='#'>
+                      Most Viewed
+                    </a>
+                  </li>
+                </ul>
+              </div>
             <div className="pt-2">
               <h4 className="fw-bold">Threads</h4>
             </div>
