ec88fd365096b60dd762fc5a874f2ff259cea882
Md. Shariar Islam Shuvo <shariaislamshuvo@gmail.com>
2023-09-23 14:41:14 +0600

    Global Variables added for wasy movement between pages (2) -shuvo

--- back-end/medi-help-back-end/src/main/java/com/amakakeru/medihelpbackend/dao/ThreadRepo.java
+++ back-end/medi-help-back-end/src/main/java/com/amakakeru/medihelpbackend/dao/ThreadRepo.java
@@ -1,7 +1,10 @@
 package com.amakakeru.medihelpbackend.dao;
 
 import com.amakakeru.medihelpbackend.entity.Thread;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ThreadRepo extends JpaRepository<Thread, String> {
+public interface ThreadRepo extends JpaRepository<Thread, Long> {
+    Page<Thread> findByUploaderIdAndThreadDateTxt(String uploaderId, String threadDateTxt, Pageable pageable);
 }
--- back-end/medi-help-back-end/src/main/java/com/amakakeru/medihelpbackend/entity/Thread.java
+++ back-end/medi-help-back-end/src/main/java/com/amakakeru/medihelpbackend/entity/Thread.java
@@ -1,11 +1,8 @@
 package com.amakakeru.medihelpbackend.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.sql.Timestamp;
 import java.util.Date;
 
 @Entity
@@ -15,7 +12,7 @@ public class Thread {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "thread_id")
-    private Integer threadId;
+    private Long threadId;
 
     @Column(name = "uploader_id")
     private String uploaderId;
